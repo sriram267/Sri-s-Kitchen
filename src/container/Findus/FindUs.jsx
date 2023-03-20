3889f3b8591ef55ae115a24e6a61974993774c92
sriram <vssriram7@gmail.com>
2023-03-20 19:23:04 +0530

    .

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -5,7 +5,7 @@ import { images } from '../../constants';
 
 const FindUs = () => (
   <>
-    <div className="app__bg app__wrapper section__padding" id="contact">
+    <div className="app__bg app__wrapper section__padding">
       <div className="app__wrapper_info">
         <SubHeading title="Contact" />
         <h1 className="headtext__cormorant" style={{ marginBottom: '3rem' }}>Find Us</h1>
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -7,7 +7,7 @@ import './SpecialMenu.css';
 
 const SpecialMenu = () => (
   <>
-    <div className="app__specialMenu flex__center section__padding" id="menu">
+    <div className="app__specialMenu flex__center section__padding">
       <div className="app__specialMenu-title">
         <SubHeading 
           tite="Menu that fits you palatte"/>
